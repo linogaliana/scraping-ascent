258ab3878c55e1b30d451f2fd8a0c2c864d76b2b
linogaliana <lino.galiana@insee.fr>
2024-03-11 20:29:12 +0000

    Avance recup gpx

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import time
 import pandas as pd
+import geopandas as gpd
 
 from scraping import (
     parse_liste_col,
-    extract_info_col
+    extract_info_col,
+    get_gpx_from_url
 )
 
 # Fetch the webpage content
@@ -11,14 +13,25 @@ url_table_page = 'https://www.cols-cyclisme.com/alpes-du-nord/liste-r1.htm'
 
 
 # Parse the list of cols and get the DataFrame
-df = parse_liste_col(url_table_page)
+#df = parse_liste_col(url_table_page)
+#df.to_parquet("liste.parquet")
+
+
+df = pd.read_parquet("liste.parquet")
 
 # Initialize an empty DataFrame to hold all details
 details_df = pd.DataFrame()
+traces = gpd.GeoDataFrame()
 
 # Iterate over each row in the DataFrame
 for index, row in df.head(10).iterrows():
     col_url = row['href']
     col_info_df = extract_info_col(col_url, id=index)
+    col_info_df['url'] = df['GPX']
+    trace = get_gpx_from_url(row['GPX'])
     details_df = pd.concat([details_df, col_info_df], ignore_index=True)
+    traces = pd.concat([traces, trace])
     time.sleep(1)  # Sleep for 1 second between requests
+
+
+essai = traces.merge(details_df, on="url")
--- scraping.py
+++ scraping.py
@@ -1,63 +1,102 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-
+import re
+import geopandas as gpd
+import gpxpy
 
 def parse_liste_col(url):
     url_root = "https://www.cols-cyclisme.com"
     response = requests.get(url)
     webpage = response.content
 
-    # Parse the HTML content
-    soup = BeautifulSoup(webpage, 'html.parser')
-
-    # Find the table by its characteristics or location in the webpage
-    table = soup.find_all('table')[0]
-
-    # Adjust the headers to include the 'Href' column
-    headers = ['href'] # Initialize with 'Href' as the first column
-    for th in table.find_all('th'):
-        headers.append(th.text.strip())
-
-    # Extract the rows and hrefs
+    soup = BeautifulSoup(webpage, "html.parser")
+    table = soup.find_all("table")[0]
+    headers = ["href"] + [th.text.strip() for th in table.find_all("th")]
     data_rows = []
-    for row in table.find_all('tr')[1:]:  # skip the header row
-        onclick_attr = row.get('onclick', '')
+
+    for row in table.find_all("tr")[1:]:  # skip the header row
+        onclick_attr = row.get("onclick", "")
         href = onclick_attr.split("'")[1] if onclick_attr else None
-        cols = row.find_all('td')
-        data_row = [
-            url_root + href] + [col.text.strip() for col in cols]  # Include href as the first item in data_row
+        cols = row.find_all("td")
+        data_row = [url_root + href] + [col.text.strip() for col in cols]
+
+        # Fetch the GPX data for the climb
+        climb_page_response = requests.get(url_root + href)
+        climb_soup = BeautifulSoup(climb_page_response.content, "html.parser")
+        script_tag = climb_soup.find("script", text=re.compile("var gpx"))
+        gpx_file_location = (
+            re.search("var gpx = '(.+?)';", script_tag.string).group(1)
+            if script_tag
+            else None
+        )
+
+        # Append the GPX data to the row
+        data_row.append(gpx_file_location)
         data_rows.append(data_row)
 
-    # Create a DataFrame
+    # Adjust the headers to include the 'GPX' column
+    headers.append("GPX")
     df = pd.DataFrame(data_rows, columns=headers)
-    df.reset_index().rename({"index": "id"}, axis = "columns")
-    df = df.drop("Détails", axis = "columns")
+    df["GPX"] = url_root + df["GPX"]
+
     return df
 
+
 # URL of the page to scrape
-def extract_info_col(url='https://www.cols-cyclisme.com/arves-et-grandes-rousses/france/auris-en-oisans-depuis-bourg-d-oisans-c2043.htm', id=1):
+def extract_info_col(
+    url="https://www.cols-cyclisme.com/arves-et-grandes-rousses/france/auris-en-oisans-depuis-bourg-d-oisans-c2043.htm",
+    id=1,
+):
     response = requests.get(url)
     webpage = response.content
-    soup = BeautifulSoup(webpage, 'html.parser')
+    soup = BeautifulSoup(webpage, "html.parser")
 
-    details_infos = soup.find('div', id='detail-infos')
+    details_infos = soup.find("div", id="detail-infos")
     informations = {}
-    for tr in details_infos.find_all('tr'):
-        key = tr.find_all('td')[0].text.strip()
-        value = tr.find_all('td')[1].text.strip()
+    for tr in details_infos.find_all("tr"):
+        key = tr.find_all("td")[0].text.strip()
+        value = tr.find_all("td")[1].text.strip()
         informations[key] = value
 
     # Attempt to find the 'imgprofil' element, handle cases where it does not exist
-    profil_img_element = soup.find('p', id='imgprofil')
-    if profil_img_element and profil_img_element.find('img'):
-        profil_image_url = profil_img_element.find('img')["data-src"]
+    profil_img_element = soup.find("p", id="imgprofil")
+    if profil_img_element and profil_img_element.find("img"):
+        profil_image_url = profil_img_element.find("img")["data-src"]
     else:
-        profil_image_url = "Not available"  # Provide a default value or perform alternative actions
+        profil_image_url = (
+            "Not available"  # Provide a default value or perform alternative actions
+        )
 
-    informations['Profil Image URL'] = profil_image_url
+    informations["Profil Image URL"] = profil_image_url
 
     df_col = pd.DataFrame([informations])
-    df_col['id'] = id
+    df_col["id"] = id
     df_col.columns = df_col.columns.str.replace(":", "").str.strip()
     return df_col
+
+
+import gpxpy
+
+
+def get_gpx_from_url(url):
+    filename = url.rsplit("/", maxsplit=1)[-1]
+    with open(f"{filename}.gpx", "wb") as f:
+        f.write(requests.get(url).content)
+
+    gpx_file = open(f"{filename}.gpx", "r")
+    gpx = gpxpy.parse(gpx_file)
+
+    points = gpx.tracks[0].segments[0].points
+
+    longitude = [point.longitude for point in points]
+    latitude = [point.latitude for point in points]
+    altitude = [point.elevation for point in points]
+
+    df = pd.DataFrame({"lon": longitude, "lat": latitude, "alt": altitude})
+    gdf = gpd.GeoDataFrame(
+        df, geometry=gpd.points_from_xy(df.lon, df.lat), crs="EPSG:4326"
+    )
+    gdf["url"] = url
+    gdf = gdf.drop(["lon", "lat"], axis="columns")
+    return gdf
